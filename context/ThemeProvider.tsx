5936e3ef1970255eadf4e4e64da27ffc02ad9ab8
militsa-tsvetkova <militsa.tsvetkova@ft.com>
2024-04-06 17:31:47 +0300

    added global theme context

--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -1,5 +1,11 @@
 "use client";
-import { createContext, ReactNode, useContext, useState } from "react";
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 interface ThemeContextType {
   mode: string;
   setMode: (mode: string) => void;
@@ -7,21 +13,21 @@ interface ThemeContextType {
 const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 
 export function ThemeProvider({ children }: { children: ReactNode }) {
-  const [mode, setMode] = useState("light");
+  const [mode, setMode] = useState("");
 
   const handleChange = () => {
     if (mode === "dark") {
-      setMode("light");
+      // setMode("light");
       document.documentElement.classList.add("light");
     } else {
-      setMode("dark");
+      // setMode("dark");
       document.documentElement.classList.add("dark");
     }
   };
 
-  //   useEffect(() => {
-  //     handleChange();
-  //   }, [mode]);
+  useEffect(() => {
+    handleChange();
+  }, [mode]);
   return (
     <ThemeContext.Provider value={{ mode, setMode }}>
       {children}
