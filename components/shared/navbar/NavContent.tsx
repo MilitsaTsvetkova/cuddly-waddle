b5e89ab793c958b10fc82f6044aee484e92da1bd
militsa-tsvetkova <militsa.tsvetkova@ft.com>
2024-04-07 12:32:47 +0300

    added Nav content

--- components/shared/navbar/NavContent.tsx
+++ components/shared/navbar/NavContent.tsx
@@ -1,5 +1,42 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { sidebarLinks } from "../../../constants";
+import { SheetClose } from "../../ui/sheet";
+
 const NavContent = () => {
-  return <div>NavContent</div>;
+  const path = usePathname();
+
+  return (
+    <section className="flex h-full flex-col gap-6 pt-16">
+      {sidebarLinks.map((link, index) => {
+        const isActive =
+          (path.includes(link.route) && link.route.length > 1) ||
+          path === link.route;
+        return (
+          <SheetClose key={index} asChild>
+            <Link
+              href={link.route}
+              className={`${isActive ? "primary-gradient rounded-lg text-gray-900" : "text-dark300_light900"} 
+              flex items-center justify-start gap-4 bg-transparent p-4`}
+            >
+              <Image
+                src={link.imgURL}
+                alt={link.label}
+                width={20}
+                height={20}
+                className={`${isActive ? "" : "invert-colors"}`}
+              />
+              <p className={`${isActive ? "base-bold" : "base-medium"}`}>
+                {link.label}
+              </p>
+            </Link>
+          </SheetClose>
+        );
+      })}
+    </section>
+  );
 };
 
 export default NavContent;
