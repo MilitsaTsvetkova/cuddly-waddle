83653eac1d763c44df1f25a068e0222a372d5fdd
militsa-tsvetkova <militsa.tsvetkova@ft.com>
2024-04-29 21:05:18 +0300

    Added Text Editor and Tags

---
+++ components/editor/TextEditor.tsx
@@ -0,0 +1,43 @@
+import { Editor } from "@tinymce/tinymce-react";
+import { useRef } from "react";
+
+export default function TextEditor() {
+  const editorRef = useRef(null);
+
+  return (
+    <Editor
+      apiKey={process.env.NEXT_PUBLIC_TINY_API_KEY}
+      onInit={(_evt, editor) => {
+        // @ts-ignore
+        editorRef.current = editor;
+      }}
+      initialValue="<p>This is the initial content of the editor.</p>"
+      init={{
+        height: 350,
+        menubar: false,
+        plugins: [
+          "advlist",
+          "autolink",
+          "lists",
+          "link",
+          "image",
+          "charmap",
+          "preview",
+          "anchor",
+          "searchreplace",
+          "visualblocks",
+          "fullscreen",
+          "insertdatetime",
+          "media",
+          "table",
+          "codesample",
+        ],
+        toolbar:
+          "undo redo | " +
+          "codesample | bold italic forecolor | alignleft aligncenter " +
+          "alignright alignjustify | bullist numlist",
+        content_style: "body { font-family:Inter; font-size:16px }",
+      }}
+    />
+  );
+}
--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -11,9 +11,12 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
+import Image from "next/image";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { questionSchema } from "../../lib/validations/questionSchema";
+import TextEditor from "../editor/TextEditor";
+import { Badge } from "../ui/badge";
 
 export function Question() {
   // 1. Define your form.
@@ -32,6 +35,38 @@ export function Question() {
     // ✅ This will be type-safe and validated.
     console.log(values);
   }
+
+  const handleKeyDownEvent = (
+    e: React.KeyboardEvent<HTMLInputElement>,
+    field: any
+  ) => {
+    if (e.key === "Enter" && field.name === "tags") {
+      e.preventDefault();
+      const tagInput = e.target as HTMLInputElement;
+      const value = tagInput.value.trim();
+      if (value !== "") {
+        if (value.length > 15) {
+          return form.setError("tags", {
+            type: "required",
+            message: "Tag should be less than 15 characters",
+          });
+        }
+        if (!field.value.includes(value as never)) {
+          form.setValue("tags", [...field.value, value]);
+          tagInput.value = "";
+          form.clearErrors("tags");
+        }
+      } else {
+        form.trigger();
+      }
+    }
+  };
+  const handleTagRemoval = (tag: string, field: any) => {
+    form.setValue(
+      "tags",
+      field.value.filter((t: string) => t !== tag)
+    );
+  };
   return (
     <Form {...form}>
       <form
@@ -70,7 +105,7 @@ export function Question() {
                 <span className="text-primary/50">*</span>
               </FormLabel>
               <FormControl className="mt-3.5">
-                {/* TODO: Add a Textarea component */}
+                <TextEditor />
               </FormControl>
               <FormDescription>
                 Introduce the problem and expand on what you put in the title.
@@ -89,11 +124,33 @@ export function Question() {
                 Tags<span className="text-primary/50">*</span>
               </FormLabel>
               <FormControl className="mt-3.5">
-                <Input
-                  placeholder="Add tags..."
-                  {...field}
-                  className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border"
-                />
+                <>
+                  <Input
+                    placeholder="Add tags..."
+                    onKeyDown={(e) => handleKeyDownEvent(e, field)}
+                    className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border"
+                  />
+                  {field.value.length > 0 && (
+                    <div className="flex-start mt-2.5 gap-2.5">
+                      {field.value.map((tag: any) => (
+                        <Badge
+                          key={tag}
+                          onClick={() => handleTagRemoval(tag, field)}
+                          className="subtle-medium background-light800_dark300 text-dark400_light500 flex items-center justify-center gap-2 rounded-md border-none px-4 py-2 capitalize"
+                        >
+                          {tag}
+                          <Image
+                            src="/assets/icons/close.svg"
+                            alt="Close Icon"
+                            width={12}
+                            height={12}
+                            className="cursor-pointer invert-0 dark:invert"
+                          />
+                        </Badge>
+                      ))}
+                    </div>
+                  )}
+                </>
               </FormControl>
               <FormDescription className="body-regular mt-2.5 text-gray-500">
                 Add up to 5 tags to describe what your question is about. Start
