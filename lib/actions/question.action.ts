560a2c8c34001860fc5b9ad9795c0dbed843e93b
militsa-tsvetkova <militsa.tsvetkova@ft.com>
2024-05-04 10:48:13 +0300

    sort questions by createdAt date

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -41,7 +41,8 @@ export async function getQuestions(params: GetQuestionsParams) {
     connectToDatabase();
     const questions = await Question.find({})
       .populate({ path: "tags", model: Tag })
-      .populate({ path: "author", model: User });
+      .populate({ path: "author", model: User })
+      .sort({ createdAt: -1 });
     return { questions };
   } catch (e) {
     console.log(e);
