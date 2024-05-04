15460602f147620b73ad167f65917a2bce13ae77
militsa-tsvetkova <militsa.tsvetkova@ft.com>
2024-05-04 11:48:19 +0300

    expose routes for clerk webhooks

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,14 +1,71 @@
 "use server";
 
+import console from "console";
+import { revalidatePath } from "next/cache";
+
+import Question from "../../database/question.model";
 import User from "../../database/user.model";
 import { connectToDatabase } from "../mongoose";
+import {
+  CreateUserParams,
+  DeleteUserParams,
+  GetUserByIdParams,
+  UpdateUserParams,
+} from "./shared.types";
 
-export async function getUserById(params: any) {
+export async function getUserById(params: GetUserByIdParams) {
   try {
     connectToDatabase();
     const { userId } = params;
     const user = await User.findOne({ clerkId: userId });
     return user;
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+}
+
+export async function createUser(userData: CreateUserParams) {
+  try {
+    connectToDatabase();
+    const newUser = await User.create(userData);
+    return newUser;
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+}
+export async function updateUser(params: UpdateUserParams) {
+  try {
+    connectToDatabase();
+    const { clerkId, updateData, path } = params;
+    await User.findByIdAndUpdate({ clerkId }, updateData, {
+      new: true,
+    });
+    revalidatePath(path);
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+}
+export async function deleteUser(params: DeleteUserParams) {
+  try {
+    connectToDatabase();
+    const { clerkId } = params;
+    const user = await User.findOneAndDelete({ clerkId });
+    if (!user) throw new Error("User not found");
+
+    // DELETE user questions
+    const userQuestionId = await Question.find({
+      author: user._id,
+    }).distinct("_id");
+
+    await Question.deleteMany({ author: user._id });
+
+    // TODO: DELETE user answers/comments
+
+    const deletedUser = await User.findByIdAndDelete(user._id);
+    return deletedUser;
   } catch (e) {
     console.log(e);
     throw e;
