4697b28a913e71917335b3478bf07282fa74e161
militsa-tsvetkova <militsa.tsvetkova@ft.com>
2024-05-04 14:54:23 +0300

    add all public routes

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,18 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware({ publicRoutes: ["/", "/api/webhooks"] });
+export default authMiddleware({
+  publicRoutes: [
+    "/",
+    "/api/webhooks",
+    "/question/:id",
+    "/tags",
+    "/tags/:id",
+    "/profile/:id",
+    "/community",
+    "/jobs",
+  ],
+  ignoredRoutes: ["/api/webhooks", "/api/chatgpt"],
+});
 
 export const config = {
   matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
