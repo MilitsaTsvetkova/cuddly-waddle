235c1a054852da67d1ec889758f65320a59d5ec2
militsa-tsvetkova <militsa.tsvetkova@ft.com>
2024-05-12 17:55:28 +0300

    added answer model

---
+++ database/answer.model.ts
@@ -0,0 +1,23 @@
+import { Document, model, models, Schema } from "mongoose";
+
+export interface iAnswer extends Document {
+  content: string;
+  upvotes: Schema.Types.ObjectId[];
+  downvotes: Schema.Types.ObjectId[];
+  author: Schema.Types.ObjectId;
+  question: Schema.Types.ObjectId;
+  createdAt: Date;
+}
+
+const AnswerSchema = new Schema({
+  content: { type: String, required: true },
+  upvotes: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  downvotes: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  author: { type: Schema.Types.ObjectId, ref: "User" },
+  question: [{ type: Schema.Types.ObjectId, ref: "Question" }],
+  createdAt: { type: Date, default: Date.now },
+});
+
+const Answer = models.Answer || model<iAnswer>("Answer", AnswerSchema);
+
+export default Answer;
