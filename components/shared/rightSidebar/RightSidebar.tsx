8bb5d62d8999023c4604eebdd5bde9ccba57a30f
militsa-tsvetkova <militsa.tsvetkova@ft.com>
2024-05-19 11:30:45 +0300

    fix tailwind error

--- components/shared/rightSidebar/RightSidebar.tsx
+++ components/shared/rightSidebar/RightSidebar.tsx
@@ -29,12 +29,12 @@ const RightSidebar = () => {
   return (
     <section
       className="background-light900_dark200 
-        light-border shadow-light-300 
-        custom-scrollbar 
-        sticky right-0 
-        top-0 flex h-screen w-[350px]
-        flex-col overflow-y-auto border-l 
-        p-6 pt-36 
+        light-border custom-scrollbar 
+        sticky 
+        right-0 top-0 
+        flex h-screen w-[350px] flex-col
+        overflow-y-auto border-l p-6 
+        pt-36 shadow-gray-300 
         dark:shadow-none
         max-xl:hidden
         "
