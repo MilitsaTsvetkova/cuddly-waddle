6838414da0344febcac468e3c133bd471e6d7983
militsa-tsvetkova <militsa.tsvetkova@ft.com>
2024-05-21 21:32:39 +0300

    manage HomeFilters filter state

--- components/filters/HomeFilters.tsx
+++ components/filters/HomeFilters.tsx
@@ -1,16 +1,39 @@
 "use client";
+import { useRouter, useSearchParams } from "next/navigation";
 import { useState } from "react";
 import { HomePageFilters } from "../../constants/filters";
+import { formUrlQuery } from "../../lib/url";
 import { Button } from "../ui/button";
 
-const HomeFilters = () => {
+const HomeFilters = (item: string) => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
   const [active, setActive] = useState("");
+  const handleTypeClick = (item: string) => {
+    if (active === item) {
+      setActive("");
+      const newUrl = formUrlQuery({
+        key: "filter",
+        params: searchParams.toString(),
+        value: null,
+      });
+      router.push(newUrl, { scroll: false });
+    } else {
+      setActive(item);
+      const newUrl = formUrlQuery({
+        key: "filter",
+        params: searchParams.toString(),
+        value: item.toLowerCase(),
+      });
+      router.push(newUrl, { scroll: false });
+    }
+  };
   return (
     <div className="mt-10 hidden flex-wrap gap-3 md:flex">
       {HomePageFilters.map((filter) => (
         <Button
           key={filter.value}
-          onClick={() => setActive(filter.value)}
+          onClick={() => handleTypeClick(filter.value)}
           className={`body-medium rounded-lg p-6 py-3 capitalize shadow-none 
           ${active === filter.value ? "bg-gray-300 text-orange-500" : "bg-gray-100 text-gray-400 hover:bg-gray-300 dark:bg-gray-800 dark:hover:bg-zinc-400"}`}
         >
