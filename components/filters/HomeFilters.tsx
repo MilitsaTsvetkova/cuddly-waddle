c28b69eae16b82bcde5c7c1268f1358bf99d13a9
militsa-tsvetkova <militsa.tsvetkova@ft.com>
2024-06-08 20:22:54 +0300

    fix: amend hover effect color on home filters on dark screen

--- components/filters/HomeFilters.tsx
+++ components/filters/HomeFilters.tsx
@@ -36,7 +36,7 @@ const HomeFilters = () => {
           key={filter.value}
           onClick={() => handleTypeClick(filter.value)}
           className={`body-medium rounded-lg p-6 py-3 capitalize shadow-none 
-          ${active === filter.value ? "bg-gray-300 text-orange-500" : "bg-gray-100 text-gray-400 hover:bg-gray-300 dark:bg-gray-800 dark:hover:bg-zinc-400"}`}
+          ${active === filter.value ? "bg-gray-300 text-orange-500" : "bg-gray-100 text-gray-400 hover:bg-gray-300 dark:bg-gray-800 dark:hover:bg-gray-200"}`}
         >
           {filter.name}
         </Button>
